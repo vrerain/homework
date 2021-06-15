eb6f622c3096adc1075ea58e354319f1f6eb2955
Crossing6 <18226171016@163.com>
2021-06-15 16:45:45 +0800

    测试推送

---
+++ homework/homework/target/classes/webapp/static/js/script.js
@@ -0,0 +1,33 @@
+const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const firstForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
+
+signInBtn.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
+});
+
+signUpBtn.addEventListener("click", () => {
+    container.classList.add("right-panel-active");
+});
+
+function check() {
+    var p1 = form1.password.value;
+    var p2 = form1.repassword.value;
+    var p3 = form1.serialNumber.value;
+    if (p3.equals("123456")) {
+        if (p1 == p2) {
+            alert("注册成功");
+            firstForm.submit();
+        } else {
+            alert("两次密码输入不一致,请重新输入");
+        }
+    } else {
+        alert("注册码未认证，请重新输入")
+    } 
+}
+
+
+// fistForm.addEventListener("submit", (e) => e.preventDefault());
+// secondForm.addEventListener("submit", (e) => e.preventDefault());
